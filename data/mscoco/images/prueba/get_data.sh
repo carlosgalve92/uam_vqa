6f84f23b550f3191f8c2e638b3fa4a8405610182
carlosgalve92 <carlos.galve92@gmail.com>
2021-11-01 18:16:31 +0100

    update id of downloads

--- data/mscoco/images/prueba/get_data.sh
+++ data/mscoco/images/prueba/get_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1G1dJSHDFKBFltmhluDeq-bTKOqUDQ4vA' -O ./data/mscoco/images/prueba/prueba.tar.gz
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1UFy0x1ZLZNeEpG7meMQB_SPzAtSwQA7o' -O ./data/mscoco/images/prueba/prueba.tar.gz
 tar -xzvf ./data/mscoco/images/prueba/prueba.tar.gz -C ./data/mscoco/images/prueba
 rm -R ./data/mscoco/images/prueba/prueba.tar.gz
-mv ./data/mscoco/images/prueba/prueba/* ./data/mscoco/images/prueba/
+find ./data/mscoco/images/prueba/prueba/ -name "*.jpg" -exec mv {} ./data/mscoco/images/prueba/ \;
 rm -R ./data/mscoco/images/prueba/prueba
--- data/mscoco/images/train2014/get_data.sh
+++ data/mscoco/images/train2014/get_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1c4OvoZSKkK88VJrFJf8jPBGXi9kAAwA_' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1c4OvoZSKkK88VJrFJf8jPBGXi9kAAwA_" -O ./data/mscoco/images/train2014/train2014.tar.gz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1JV8la7E1Pm4KJcG50b2aTAcQqjIgP-aL' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JV8la7E1Pm4KJcG50b2aTAcQqjIgP-aL" -O ./data/mscoco/images/train2014/train2014.tar.gz && rm -rf /tmp/cookies.txt
 tar -xzvf ./data/mscoco/images/train2014/train2014.tar.gz -C ./data/mscoco/images/train2014
 rm -R ./data/mscoco/images/train2014/train2014.tar.gz
 find ./data/mscoco/images/train2014/train2014/ -name "*.jpg" -exec mv {} ./data/mscoco/images/train2014/ \;
