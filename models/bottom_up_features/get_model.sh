f7608a9b69ea9ca0c60a338f7cd75fa07703f177
carlosgalve92 <carlos.galve92@gmail.com>
2022-01-08 18:15:58 +0100

    change to new name of model bottom-up

--- models/bottom_up_features/get_model.sh
+++ models/bottom_up_features/get_model.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget https://awma1-my.sharepoint.com/:u:/g/personal/yuz_l0_tn/EUKhQ3hSRv9JrrW64qpNLSIBGoOjEGCkF8zvgBP9gKax-w?download=1 -O ./models/bottom_up_features/extract-bua-caffe-r101-fix36.yaml
+wget https://awma1-my.sharepoint.com/:u:/g/personal/yuz_l0_tn/EUKhQ3hSRv9JrrW64qpNLSIBGoOjEGCkF8zvgBP9gKax-w?download=1 -O ./models/bottom_up_features/bua-caffe-frcn-r101-k36.pth
